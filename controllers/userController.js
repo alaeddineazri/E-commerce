2e3e8c62a4c8ad88ab31b4848e4d9d3ea0bbd301
azri <alaeddinazri@gmail.com>
2022-04-08 01:08:36 +0100

    JWT login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,14 @@
 // import models
-const User = require('../models/userModel');
+const User = require('../models/userModel')
+//import jsonwebtoken to generate token
+const jwt=require('jsonwebtoken')
+// import expressJwt to verify token authorization
+const expressJwt = require('express-jwt')
+
+
+
 const {errorHandler} = require('../helpers/dbErrHandler');
-const { use } = require('../routes/userRoutes');
+
 
 
 exports.Signup=(req,res)=>{
@@ -23,4 +30,32 @@ exports.Signup=(req,res)=>{
             user
         });
     });
+}
+
+//login controller
+exports.Login=(req,res)=>{ 
+    //find the user based on email
+    const {email,password}=req.body
+    User.findOne({email},(err,user)=>{
+        if(err || !user){
+            return res.status(400).json({
+                error:"User with that email does not exist. Please signup"
+            });
+        }
+        //if user is found make sure the email and password match
+        //create authenticate method in user model
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error:"Wrong password"
+            });
+        }
+        //generate a token with user id and secret
+        const token=jwt.sign({_id:user._id},process.env.JWT_SECRET);
+        //persist the token as 't' in cookie with expiry date
+        res.cookie("t",token,{expire:new Date()+7200})    //2 hours
+        //return response with user and token to frontend client
+        const {_id,name,email,role}=user
+        return res.json({token,user:{_id,name,email,role}});
+    });
+
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,11 @@
 const express = require('express')
 const router = express.Router()
 // import from controllers
-const {Signup} = require('../controllers/userController')
+const {Signup , Login } = require('../controllers/userController')
 const {userSignupValidator} = require('../validator')
 
 router.post('/signup',userSignupValidator, Signup)
+router.post('/login', Login)
 
 
 
